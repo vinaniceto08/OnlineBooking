6ec655108133e3697bb8277e9a9c6b630c8ad1bc
Marvin Aniceto <Marvin.Aniceto@genusplc.com>
2025-09-25 23:21:33 +0800

    Added API and back end db for Agoda Affiliate

---
+++ TravelTayo/TravelTayo/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using TravelTayo.Models;
+
+namespace TravelTayo.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
