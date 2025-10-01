8bdcaf693689e0bccc592c62b9bc64c9520bc15e
Marvin Aniceto <Marvin.Aniceto@genusplc.com>
2025-10-02 05:03:50 +0800

    Added settings folder

---
+++ TravelTayo/TravelTayo/Settings/BlobConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelTayo.Settings
+{
+    public static class BlobConfig
+    {
+        public static string ConnectionString { get; set; } = "Your_Azure_Blob_Connection_String";
+        public static string ContainerName { get; set; } = "referralqrcodes";
+    }
+}
